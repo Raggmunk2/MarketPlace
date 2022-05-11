56e258b69286c9ec56ef69c50c66216b3fda1ea8
Frida Jacobsson <frida_jacobsson@live.se>
2022-05-11 14:59:09 +0200

    Fixed small bugg with main controller

--- src/main/java/client/controller/MainMenuController.java
+++ src/main/java/client/controller/MainMenuController.java
@@ -10,44 +10,35 @@ import java.sql.Date;
 public class MainMenuController {
 
     private UserInterface userInterface;
-    private client.controller.RequestHandler requestHandler;
-    private LoggedInController loggedInController;
+    private RequestHandler requestHandler;
 
     public MainMenuController() {
         this.userInterface = new UserInterface();
         this.requestHandler = new client.controller.RequestHandler("127.0.0.1", 6890);
-        mainMenuHandler(userInterface.showMainMenu());
+        mainMenuHandler();
     }
 
-    private void mainMenuHandler(int input) {
-        switch (input) {
-            case 1:
-                User user = null;
-                do {
-                    user = loginUser();
-                } while (user == null);
-                new LoggedInController(user, this.userInterface, this.requestHandler);
-                break;
-            case 2:
-                String firstName = userInterface.getFirstName();
-                String lastName = userInterface.getLastName();
-                Date DoB = userInterface.getDateOfBirth();
-                String email = userInterface.getEmail();
-                String newUsername = userInterface.getUsername();
-                String newPassword = userInterface.getPassword();
-                user = new User(newUsername, newPassword, firstName, lastName, DoB, email);
-                ResponseMessage registerResponse = requestHandler.getRegisterResponse(user);
-                Boolean success = registerResponse.getSuccess();
-                if (success) {
-                    System.out.println("Welcome " + user.getUserName() + " what would you like to do now?");
+    private void mainMenuHandler() {
+        int input;
+        do {
+            input = userInterface.showMainMenu();
+            switch (input) {
+                case 1:
+                    User user = null;
+                    do {
+                        user = loginUser();
+                    } while (user == null);
                     new LoggedInController(user, this.userInterface, this.requestHandler);
-                } else {
-                    System.out.println("Sorry we could not add the user, please try again.");
-                    mainMenuHandler(userInterface.showMainMenu());
-                }
-            case 3:
-                System.exit(0);
-        }
+                    break;
+                case 2:
+                    user = register();
+                    if (user != null) {
+                        System.out.println("Welcome " + user.getUserName() + " what would you like to do now?");
+                        new LoggedInController(user, this.userInterface, this.requestHandler);
+                    }
+            }
+        }while (input != 3);
+        System.exit(0);
     }
 
     private User loginUser() {
@@ -61,5 +52,22 @@ public class MainMenuController {
             }
         }
         return loginResponse.getUser();
+    }
+
+    private User register() {
+        String firstName = userInterface.getFirstName();
+        String lastName = userInterface.getLastName();
+        Date DoB = userInterface.getDateOfBirth();
+        String email = userInterface.getEmail();
+        String newUsername = userInterface.getUsername();
+        String newPassword = userInterface.getPassword();
+        User user = new User(newUsername, newPassword, firstName, lastName, DoB, email);
+        ResponseMessage registerResponse = requestHandler.getRegisterResponse(user);
+        Boolean success = registerResponse.getSuccess();
+        if (!success) {
+            userInterface.printMessage("Sorry we could not add the user, please try again.");
+            return null;
+        }
+        return user;
     }
 }
