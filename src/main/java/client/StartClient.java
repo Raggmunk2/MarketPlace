9031ad76f115e0cc9c93bdc7b7e8a294ed187155
Raggmunk2 <76004088+Raggmunk2@users.noreply.github.com>
2022-05-16 11:26:18 +0200

    made a new branch

--- src/main/java/client/StartClient.java
+++ src/main/java/client/StartClient.java
@@ -5,6 +5,7 @@ import client.controller.MainMenuController;
 public class StartClient {
 
     public static void main(String[] args) {
+        
         MainMenuController mainMenuController = new MainMenuController();
     }
 }
